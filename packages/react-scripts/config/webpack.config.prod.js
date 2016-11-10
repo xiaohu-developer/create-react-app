5029aedf286b803a7159b094f5d70e251b770dcf
Mofe Lee <mofe@me.com>
2016-11-10 10:46:37 +0800

    update offline plugin config

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -261,7 +261,13 @@ module.exports = {
     new ManifestPlugin({
       fileName: 'asset-manifest.json'
     }),
-    new OfflinePlugin()
+    new OfflinePlugin({
+      publicPath: publicPath,
+      relativePaths: false,
+      ServiceWorker: {
+        scope: publicPath
+      }
+    })
   ],
   // Some libraries import Node modules but don't use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
